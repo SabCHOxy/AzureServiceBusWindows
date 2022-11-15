1ecc9c5b9c04a5632290ed69fcdbefb674a0fdeb
Saber CHIKHA <schikha@oxybul.com>
2022-11-15 02:51:36 +0100

    Add files project

---
+++ ServiceBusVersionW/ServiceBus.Producer/Services/IMessagePublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Producer.Services
+{
+    interface IMessagePublisher
+    {
+        Task Publish<T>(T obj);
+        Task Publish(string message);
+    }
+}
---
+++ ServiceBusVersionW/ServiceBus.Producer/Services/MessagePublisher.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Producer.Services
+{
+    class MessagePublisher : IMessagePublisher
+    {
+        private readonly IQueueClient _queuedClient;
+
+        public MessagePublisher(IQueueClient queuedClient)
+        {
+            _queuedClient = queuedClient;
+        }
+        public Task Publish<T>(T obj)
+        {
+            var objAsText = JsonConvert.SerializeObject(obj);
+            var msg = new Message(Encoding.UTF8.GetBytes(objAsText));
+            return _queuedClient.SendAsync(msg);
+        }
+
+        public Task Publish(string message)
+        {
+            var msg = new Message(Encoding.UTF8.GetBytes(message));
+            return _queuedClient.SendAsync(msg);
+
+        }
+    }
+}
